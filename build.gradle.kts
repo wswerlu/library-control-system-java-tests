5605463f5eb5f8c80ded115216298449f8263782
Petr Popov <popovpn7@gmail.com>
2024-03-11 22:53:37 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    id("checkstyle")
+    id("pmd")
+}
+
+group = "org.library"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+checkstyle {
+    toolVersion = "10.14.0"
+    config = resources.text.fromFile("${project.projectDir}/src/main/resources/checkstyle.xml")
+}
+
+pmd {
+    toolVersion = "6.55.0"
+    isConsoleOutput = true
+    ruleSets = listOf("${project.projectDir}/src/main/resources/pmdrules.xml")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "library-control-system-java-tests"
+
