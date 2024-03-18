8d402abfedf6163a3853c31a69eae934516dbc4f
Petr Popov <popovpn7@gmail.com>
2024-03-18 21:37:22 +0300

    Add jpa config class

---
+++ src/main/java/org/library/config/database/LibraryControlSystemJpaConfig.java
@@ -0,0 +1,66 @@
+package org.library.config.database;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "org.library.databases.library_control_system.repository",
+        entityManagerFactoryRef = "libraryControlSystemEntityManagerFactory",
+        transactionManagerRef = "libraryControlSystemTransactionManager")
+public class LibraryControlSystemJpaConfig {
+
+    @Bean
+    public DataSource dsLibraryControlSystem() {
+        return DataSourceBuilder.create()
+                .url("jdbc:h2:tcp://localhost:9092/mem:library_control_system")
+                .username("test")
+                .password("test")
+                .driverClassName("org.h2.Driver")
+                .build();
+    }
+
+    private Map<String, String> additionalProperties() {
+        Map<String, String> configuration = new HashMap<>();
+        configuration.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        return configuration;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean libraryControlSystemEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("dsLibraryControlSystem") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("org.library.databases.library_control_system.entity")
+                .properties(additionalProperties())
+                .build();
+    }
+
+    @Bean
+    public EntityManagerFactoryBuilder entityManagerFactoryBuilder() {
+        return new EntityManagerFactoryBuilder(
+                new HibernateJpaVendorAdapter(),
+                new HashMap<>(),
+                null);
+    }
+
+    @Bean
+    public PlatformTransactionManager libraryControlSystemTransactionManager(
+            @Qualifier("libraryControlSystemEntityManagerFactory") EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+}
