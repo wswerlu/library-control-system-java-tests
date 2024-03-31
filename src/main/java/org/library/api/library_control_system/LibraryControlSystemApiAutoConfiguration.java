04782c2a2b1f09ba9a357b5f6eefbdad36dee74d
Petr Popov <popovpn7@gmail.com>
2024-03-31 17:35:58 +0300

    Add classes for working with author API

---
+++ src/main/java/org/library/api/library_control_system/LibraryControlSystemApiAutoConfiguration.java
@@ -0,0 +1,9 @@
+package org.library.api.library_control_system;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class LibraryControlSystemApiAutoConfiguration {
+}
