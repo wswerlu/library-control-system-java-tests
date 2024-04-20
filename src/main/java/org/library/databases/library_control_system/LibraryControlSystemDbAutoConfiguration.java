ad108576f2ad6fa159ba41b7e1b6dc95028d2e1a
Petr Popov <popovpn7@gmail.com>
2024-04-20 16:15:39 +0300

    Rename LibraryControlSystemDataBaseAutoConfiguration class to LibraryControlSystemDbAutoConfiguration

--- src/main/java/org/library/databases/library_control_system/LibraryControlSystemDbAutoConfiguration.java
+++ src/main/java/org/library/databases/library_control_system/LibraryControlSystemDbAutoConfiguration.java
@@ -1,10 +1,12 @@
 package org.library.databases.library_control_system;
 
 import org.library.config.database.LibraryControlSystemJpaConfig;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({LibraryControlSystemJpaConfig.class})
-public class LibraryControlSystemDataBaseAutoConfiguration {
+@ComponentScan
+public class LibraryControlSystemDbAutoConfiguration {
 }
