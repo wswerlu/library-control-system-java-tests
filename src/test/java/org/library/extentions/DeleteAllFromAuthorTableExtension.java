d7fdc71660c2f8ce2f6e4b1417e661a5bacaccb0
Petr Popov <popovpn7@gmail.com>
2024-04-20 17:00:04 +0300

    Update DeleteAllFromAuthorTableExtension

--- src/test/java/org/library/extentions/DeleteAllFromAuthorTableExtension.java
+++ src/test/java/org/library/extentions/DeleteAllFromAuthorTableExtension.java
@@ -2,16 +2,16 @@ package org.library.extentions;
 
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.library.databases.library_control_system.repository.AuthorRepository;
+import org.library.databases.library_control_system.service.AuthorDbService;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 public class DeleteAllFromAuthorTableExtension implements AfterEachCallback {
 
     @Override
-    public void afterEach(ExtensionContext extensionContext) throws Exception {
-        AuthorRepository authorRepository = SpringExtension
-                .getApplicationContext(extensionContext).getBean(AuthorRepository.class);
+    public void afterEach(ExtensionContext extensionContext) {
+        AuthorDbService authorDbService = SpringExtension
+                .getApplicationContext(extensionContext).getBean(AuthorDbService.class);
 
-        authorRepository.deleteAll();
+        authorDbService.deleteAllAuthors();
     }
 }
